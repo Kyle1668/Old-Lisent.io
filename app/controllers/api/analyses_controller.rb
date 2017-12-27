b6bae0b6e16b931d00fd5d34eeea23acf3288c39
Kyle1668 <kyledevinobrien1@gmail.com>
2017-12-27 15:35:05 -0800

    added find by id

--- app/controllers/api/analyses_controller.rb
+++ app/controllers/api/analyses_controller.rb
@@ -2,4 +2,9 @@ class Api::AnalysesController < ApplicationController
   def index
     render json: Analysis.all
   end
+
+  def show
+    list = Analysis.find(params[:id])
+    render json: list
+  end
 end
