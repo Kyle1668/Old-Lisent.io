f497779d98db2de05a54bd2ccfac42866f02ac12
Kyle1668 <kyledevinobrien1@gmail.com>
2018-01-07 20:08:21 -0800

    updated nlp script to include neutral classification

--- NLP/get_sentiment.py
+++ NLP/get_sentiment.py
@@ -38,7 +38,7 @@ def get_sentiment(argued_text):
 
 
 def convert_data_to_json(sentiment_data):
-    if abs(sentiment_data.p_pos - sentiment_data.p_neg) is .00001:
+    if str(sentiment_data.p_pos) is str(sentiment_data.p_neg):
         return_classification = "Neutral"
     elif sentiment_data.classification is "pos":
         return_classification = "Positive"
